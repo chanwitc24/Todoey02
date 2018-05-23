cd2ebaeb5bbb53e529eb58be676c33f6db03b746
Chanwit Ch <chanwitchummung@chanwits-macbook-pro.local>
2018-05-23 17:29:13 +0700

    Convert to MVC design pattern.

---
+++ Todoey02/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey02
+//
+//  Created by Chanwit Chummung on 23/5/2561 BE.
+//  Copyright © 2561 Chanwit Chummung. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
